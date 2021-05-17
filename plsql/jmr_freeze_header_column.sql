ea510a7a8f728e30e58773cb152fa6e7a9f37fa3
aflorestorres <aftorres02@gmail.com>
2021-05-16 23:08:26 -0500

    allow multiple columns to freeze, 3 new options for the plugin * testing on apex 18 and 20.1, there are some bugs: # border for the columns does not appear correctly # Background borders for 20.1

--- plsql/jmr_freeze_header_column.sql
+++ plsql/jmr_freeze_header_column.sql
@@ -1,24 +1,40 @@
 create or replace
 function jmr_freeze_header_column (
     p_dynamic_action in apex_plugin.t_dynamic_action
-  , p_plugin         in apex_plugin.t_plugin 
-) 
+  , p_plugin         in apex_plugin.t_plugin
+)
 return apex_plugin.t_dynamic_action_render_result
 is
   l_result apex_plugin.t_dynamic_action_render_result;
 
-  l_attr_default_height_ind varchar2(1)   := p_dynamic_action.attribute_01;
-  l_attr_custom_height      varchar2(100) := p_dynamic_action.attribute_02;
-  l_attr_bg_color           varchar2(20)  := nvl(p_dynamic_action.attribute_03, '#FFFFF');
+  l_attr_nro_columns        number      := p_dynamic_action.attribute_01;
+  l_attr_border_header_yn   varchar2(1) := p_dynamic_action.attribute_02;
+  l_attr_border_columns_yn  varchar2(1) := p_dynamic_action.attribute_03;
+  l_attr_custom_height      varchar2(100) := '700px';
+  l_attr_bg_color           varchar2(20)  := '#FFFFF';
+  l_crlf                    char(2) := chr(13)||chr(10);
 
 begin
 
-  l_result.attribute_02        := nvl(l_attr_custom_height, '700px');
-  l_result.attribute_03        := l_attr_bg_color;
+  l_result.attribute_01        := l_attr_nro_columns;
+  l_result.attribute_02        := l_attr_border_header_yn;
+  l_result.attribute_03        := l_attr_border_columns_yn;
 
   l_result.javascript_function := '
     function(){
-      insumTk.freezer.coolReport(this.triggeringElement);
+      insumTk.freezer.coolReport(this.triggeringElement,' || '{'
+      || l_crlf ||  apex_javascript.add_attribute(p_name      => 'nro_columns'
+                                                , p_value     => l_attr_nro_columns
+                                                , p_add_comma => TRUE)
+      || l_crlf ||  apex_javascript.add_attribute(p_name      => 'border_header_yn'
+                                                , p_value     => l_attr_border_header_yn
+                                                , p_add_comma => TRUE)
+      || l_crlf ||  apex_javascript.add_attribute(p_name      => 'border_columns_yn'
+                                                , p_value     => l_attr_border_columns_yn
+                                                , p_add_comma => FALSE)
+      || '}' ||
+      '
+      );
     }';
 
     -- Set the size for ALL applicable regions after the window is resized
